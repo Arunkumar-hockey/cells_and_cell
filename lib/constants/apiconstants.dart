12d80fa7a74f6609dab103de3b7d3fb6afd80bd3
Arun Kumar K <arunkumar.hockey@gmail.com>
2022-02-02 11:23:53 +0530

    commit

--- lib/constants/apiconstants.dart
+++ lib/constants/apiconstants.dart
@@ -9,4 +9,5 @@ class APIConstants {
   static const KWalletAmount = "/api/V1/wallet/amount";
   static const KProfileEdit = "/api/V1/profile/edit";
   static const KProfileUpdate = "/api/V1/profile/update";
+  static const KSignOut = "/api/V1/sign-out";
 }
--- lib/service/APIService.dart
+++ lib/service/APIService.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:cells_and_cell/allpackages.dart';
 import 'package:cells_and_cell/constants/apiconstants.dart';
 import 'package:cells_and_cell/model/profile_edit.dart';
@@ -10,53 +9,54 @@ import 'package:cells_and_cell/model/walletamount.dart';
 import 'package:http/http.dart' as http;
 
 class APIService {
-
   final box = GetStorage();
 
-  Future getUserLogin(String email, String password) async{
+  Future getUserLogin(String email, String password) async {
     String completeUrl = APIConstants.BaseUrl + APIConstants.KLogin;
     var uri = Uri.parse(completeUrl);
 
     var response = await http.post(uri,
         headers: {"Accept": "Application/json"},
-        body: {"email": email,"password": password}
-    );
+        body: {"email": email, "password": password});
     var convertedDataToJson = jsonDecode(response.body);
     print(convertedDataToJson);
     return convertedDataToJson;
   }
 
-  Future<ProfileModel?> FetchProfile() async{
+  Future<ProfileModel?> FetchProfile() async {
     String token = box.read("token");
     String completeUrl = APIConstants.BaseUrl + APIConstants.KProfile;
     var url = Uri.parse(completeUrl);
 
-    try{
-      final response = await http.get(url,
-        headers: { 'Authorization': 'Bearer $token'},
+    try {
+      final response = await http.get(
+        url,
+        headers: {'Authorization': 'Bearer $token'},
       );
-      print('Token : ${token}');
+      print('Token : $token');
       print("====Map====${response.body.toString()}");
       if (response.statusCode == 200) {
-        ProfileModel profileModel = ProfileModel.fromJson(json.decode(response.body));
+        ProfileModel profileModel =
+            ProfileModel.fromJson(json.decode(response.body));
         return profileModel;
       } else {
         return null;
       }
-    } catch(error){
+    } catch (error) {
       print(error.toString());
       return null;
     }
   }
 
-  Future <ProfileEdit?> FetchProfileEdit() async{
+  Future<ProfileEdit?> FetchProfileEdit() async {
     String token = box.read("token");
     String completeUrl = APIConstants.BaseUrl + APIConstants.KProfileEdit;
     var url = Uri.parse(completeUrl);
 
     try {
-      final response = await http.get(url,
-        headers: { 'Authorization': 'Bearer $token'},
+      final response = await http.get(
+        url,
+        headers: {'Authorization': 'Bearer $token'},
       );
       print("====Map====${response.body.toString()}");
       if (response.statusCode == 200) {
@@ -71,111 +71,130 @@ class APIService {
     }
   }
 
-  Future ProfileUpdate(String name, String phoneNo) async{
+  Future ProfileUpdate(String name, String phoneNo) async {
     String token = box.read("token");
-    String completeUrl = APIConstants.BaseUrl + APIConstants.KProfileUpdate ;
+    String completeUrl = APIConstants.BaseUrl + APIConstants.KProfileUpdate;
     var url = Uri.parse(completeUrl);
 
     var response = await http.post(url,
-      headers: { 'Authorization': 'Bearer $token'},
-      body: {"name": name, "phone": phoneNo}
-    );
+        headers: {'Authorization': 'Bearer $token'},
+        body: {"name": name, "phone": phoneNo});
 
     var convertedDataToJson = json.decode(response.body);
     print(convertedDataToJson);
     return convertedDataToJson;
   }
 
-  Future<ViewQrCode?> FetchQRCode() async{
+  Future<ViewQrCode?> FetchQRCode() async {
     String token = box.read("token");
     String completeUrl = APIConstants.BaseUrl + APIConstants.KQRGenerate;
     var url = Uri.parse(completeUrl);
 
-    try{
-      final response = await http.get(url,
-        headers: { 'Authorization': 'Bearer $token'},
+    try {
+      final response = await http.get(
+        url,
+        headers: {'Authorization': 'Bearer $token'},
       );
-      print('Token : ${token}');
+      print('Token : $token');
       print("====Map====${response.body.toString()}");
       if (response.statusCode == 200) {
-        var viewQRCode =
-        ViewQrCode.fromJson(json.decode(response.body));
+        var viewQRCode = ViewQrCode.fromJson(json.decode(response.body));
         return viewQRCode;
       } else {
         return null;
       }
-    } catch(error){
-      return null;
+    } catch (e) {
+      throw e.toString();
     }
   }
 
-  Future <void> checkQRCode(String qrCode) async{
+  Future<void> checkQRCode(String qrCode) async {
     String token = box.read("token");
     String completeUrl = APIConstants.BaseUrl + APIConstants.KQRCheck;
     var url = Uri.parse(completeUrl);
 
-    try{
+    try {
       final response = await http.post(url,
-          headers: { 'Authorization': 'Bearer $token'},
-          body: {"password": qrCode}
-      );
+          headers: {'Authorization': 'Bearer $token'},
+          body: {"password": qrCode});
       print("====Map====${response.body.toString()}");
 
-      if(response.statusCode == 200) {
-
+      if (response.statusCode == 200) {
       } else {
         return null;
       }
-    } catch(e) {
-      return null;
+    } catch (e) {
+      throw e.toString();
     }
   }
 
-  Future<Transactions?> fetchTransactions() async{
+  Future<Transactions?> fetchTransactions() async {
     String token = box.read("token");
     String completeUrl = APIConstants.BaseUrl + APIConstants.KTransactions;
     var url = Uri.parse(completeUrl);
 
-    try{
-      final response = await http.post(url,
-        headers: { 'Authorization': 'Bearer $token'},
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Authorization': 'Bearer $token'},
       );
-      print('Token : ${token}');
+      print('Token : $token');
       print("====Map====${response.body.toString()}");
       if (response.statusCode == 200) {
-        var transactionDetails = Transactions.fromJson(json.decode(response.body));
+        var transactionDetails =
+            Transactions.fromJson(json.decode(response.body));
         return transactionDetails;
       } else {
         return null;
       }
-    } catch(error){
-      print(error.toString());
-      return null;
+    } catch (e) {
+      print(e.toString());
+      throw e.toString();
     }
   }
 
-  Future<WalletAmount?> fetchWalletAmount() async{
+  Future<WalletAmount?> fetchWalletAmount() async {
     String token = box.read("token");
     String completeUrl = APIConstants.BaseUrl + APIConstants.KWalletAmount;
     var url = Uri.parse(completeUrl);
 
-    try{
-      final response = await http.post(url,
-        headers: { 'Authorization': 'Bearer $token'},
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Authorization': 'Bearer $token'},
       );
-      print('Token : ${token}');
+      print('Token : $token');
       print("====Map====${response.body.toString()}");
       if (response.statusCode == 200) {
         var walletAmount = WalletAmount.fromJson(json.decode(response.body));
         return walletAmount;
-      }
-      else {
+      } else {
         return null;
       }
-    } catch(error){
-      print(error.toString());
-      return null;
+    } catch (e) {
+      print(e.toString());
+      throw e.toString();
     }
   }
 
+  Future SignOut() async {
+    String token = box.read("token");
+    String completeUrl = APIConstants.BaseUrl + APIConstants.KSignOut;
+    var url = Uri.parse(completeUrl);
+
+    try {
+      var response = await http.post(
+        url,
+        headers: {'Authorization': 'Bearer $token'},
+      );
+      if (response.statusCode == 200) {
+        var convertedDataToJson = json.decode(response.body);
+        return convertedDataToJson;
+      }
+      return null;
+    } catch (e) {
+      print(e.toString());
+      throw e.toString();
+    }
+  }
 }
